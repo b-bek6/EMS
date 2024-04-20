5a9e43bbb6b7faa18b2bdde0010781bf93d92b94
bibeeEnterleaf <bibekghimire.co@gmail.com>
2024-04-20 13:03:56 +0545

    profiles index

---
+++ Controllers/ProfilesController.cs
@@ -0,0 +1,32 @@
+﻿using Employee_Management_System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management_System;
+
+public class ProfilesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public ProfilesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var tasks = new ProfileViewModel();
+        var roles = await _context.Roles.OrderBy(x=>x).ToListAsync();
+        ViewBag.Roles = new SelectList(roles,"Id","Name");
+        tasks.Profiles = await _context.SystemProfiles
+            .Include("Children.Children.Children")
+            .OrderBy(x => x.Order)
+            .Where(x => x.ProfileId == null)
+            .ToListAsync();
+        return View(tasks);
+        
+
+
+
+    }
+}
---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Employee_Management_System;
+
+public class ProfileViewModel
+{
+    public ICollection<SystemProfile> Profiles { get; set; }
+    [DisplayName("Role")]
+    public string RoleId { get; set; }
+}
