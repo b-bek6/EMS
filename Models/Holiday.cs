8b6cb5d34ee9e86567d936de34a6a073b133abd4
Bibek Ghimire <binekghimire555@gmail.com>
2024-04-26 17:07:54 +0545

    employee model

---
+++ Models/Holiday.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Management_System;
+
+public class Holiday : UserAcitvity
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Description { get; set; }
+
+}
